97a4d8f17631c2ae2b066b8d4199998c9bed42f3
kataynoi <thait-rex@hotmail.com>
2020-03-28 13:33:22 +0700

    Add

--- application/views/person_bypass/index.php
+++ application/views/person_bypass/index.php
@@ -52,7 +52,6 @@
 <div class="modal fade" id="frmModal">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-
             <!-- Modal Header -->
             <div class="modal-header">
                 <h4 class="modal-title">เพิ่มประชาชนที่ เดินทางเข้า มหาสารคามผ่านจุดตรวจ</h4>
@@ -64,76 +63,129 @@
                 <input type="hidden" id="action" value="insert">
                 <input type="hidden" class="form-control" id="row_id" placeholder="ROWID" value="">
 
-                <div class="form-group">
+                <div class="form-row">
+                        <input type="hidden" class="form-control" id="id" placeholder="ID" value="">
+                    <div class="form-group col-md-3">
+                        <label for="cid">เลขบัตรประชาชน</label>
+                        <input type="text" class="form-control" id="cid" placeholder="เลขบัตรประชาชน" value="">
+                    </div>
+                </div>
 
-                    <input type="hidden" class="form-control" id="id" placeholder="ID" value=""></div>
-                <div class="form-group">
-                    <label for="cid">เลขบัตรประชาชน</label>
-                    <input type="text" class="form-control" id="cid" placeholder="เลขบัตรประชาชน" value=""></div>
-                <div class="form-group">
-                    <label for="trpre">คำนำหน้า</label>
-                    <input type="text" class="form-control" id="trpre" placeholder="คำนำหน้า" value=""></div>
-                <div class="form-group">
-                    <label for="tname">ชื่อ</label>
-                    <input type="text" class="form-control" id="tname" placeholder="ชื่อ" value=""></div>
-                <div class="form-group">
-                    <label for="tlast">สกุล</label>
-                    <input type="text" class="form-control" id="tlast" placeholder="สกุล" value=""></div>
-                <div class="form-group">
-                    <label for="birth">วันเกิด</label>
-                    <input type="text" class="form-control" id="birth" placeholder="วันเกิด" value=""></div>
-                <div class="form-group">
-                    <label for="sex">เพศ</label>
-                    <input type="text" class="form-control" id="sex" placeholder="เพศ" value=""></div>
-                <div class="form-group">
-                    <label for="addrno">บ้านเลขที่</label>
-                    <input type="text" class="form-control" id="addrno" placeholder="บ้านเลขที่" value=""></div>
-                <div class="form-group">
-                    <label for="addrmu">หมู่</label>
-                    <input type="text" class="form-control" id="addrmu" placeholder="หมู่" value=""></div>
-                <div class="form-group">
-                    <label for="addrtb">ตำบล</label>
-                    <input type="text" class="form-control" id="addrtb" placeholder="ตำบล" value=""></div>
-                <div class="form-group">
-                    <label for="addrap">อำเภอ</label>
-                    <input type="text" class="form-control" id="addrap" placeholder="อำเภอ" value=""></div>
-                <div class="form-group">
-                    <label for="addrcw">จังหวัด</label>
-                    <input type="text" class="form-control" id="addrcw" placeholder="จังหวัด" value=""></div>
-                <div class="form-group">
-                    <label for="agenow">อายุ</label>
-                    <input type="text" class="form-control" id="agenow" placeholder="อายุ" value=""></div>
-                <div class="form-group">
-                    <label for="datestamp">วันที่บันทึก</label>
-                    <input type="text" class="form-control" id="datestamp" placeholder="วันที่บันทึก" value=""></div>
-                <div class="form-group">
-                    <label for="datestamp">โทร</label>
-                    <input type="text" class="form-control" id="tel" placeholder="เบอร์โทร" value=""></div>
-                <div class="form-group">
-                    <label for="temp_check">มาจาก</label>
-                    <input type="text" class="form-control" id="form" placeholder="จังหวัด" value=""></div>
-                <div class="form-group">
-                    <label for="temp_check">เดินทางไป</label>
-                    <input type="text" class="form-control" id="to" placeholder="จังหวัด" value=""></div>
-                <div class="form-group">
-                    <label for="temp_check">อุณหภูมิปกติ</label>
-                    <input type="text" class="form-control" id="temp_check" placeholder="อุณหภูมิปกติ" value=""></div>
-                <div class="form-group">
-                    <label for="temp_result">อุณหภูมิ</label>
-                    <input type="text" class="form-control" id="temp_result" placeholder="อุณหภูมิ" value=""></div>
-                <div class="form-group">
-                    <label for="symtom1">อาการทั่วไป</label>
-                    <input type="text" class="form-control" id="symtom1" placeholder="อาการทั่วไป" value=""></div>
-                <div class="form-group">
-                    <label for="check_point">จุดตรวจ</label>
-                    <input type="hidden" class="form-control" id="check_point" placeholder="จุดตรวจ" value=""></div>
-                    <input type="text" class="form-control" id="check_point2" placeholder="จุดตรวจ" value="" disabled ></div>
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="trpre">คำนำหน้า</label>
+                        <input type="text" class="form-control" id="trpre" placeholder="คำนำหน้า" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="tname">ชื่อ</label>
+                        <input type="text" class="form-control" id="tname" placeholder="ชื่อ" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="tlast">สกุล</label>
+                        <input type="text" class="form-control" id="tlast" placeholder="สกุล" value="">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="birth">วันเกิด</label>
+                        <input type="text" class="form-control" id="birth" placeholder="วันเกิด" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="sex">เพศ</label>
+                        <input type="text" class="form-control" id="sex" placeholder="เพศ" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="addrno">บ้านเลขที่</label>
+                        <input type="text" class="form-control" id="addrno" placeholder="บ้านเลขที่" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="addrmu">หมู่</label>
+                        <input type="text" class="form-control" id="addrmu" placeholder="หมู่" value="">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="addrtb">ตำบล</label>
+                        <input type="text" class="form-control" id="addrtb" placeholder="ตำบล" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="addrap">อำเภอ</label>
+                        <input type="text" class="form-control" id="addrap" placeholder="อำเภอ" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="addrcw">จังหวัด</label>
+                        <input type="text" class="form-control" id="addrcw" placeholder="จังหวัด" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="agenow">อายุ</label>
+                        <input type="text" class="form-control" id="agenow" placeholder="อายุ" value="">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-md-3 ">
+                        <label for="datestamp">วันที่บันทึก</label>
+                        <input type="text" class="form-control" id="datestamp" placeholder="วันที่บันทึก" value="">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="datestamp">โทร</label>
+                        <input type="text" class="form-control" id="tel" placeholder="เบอร์โทร" value="">
+                    </div>
+
+                    <div class="form-group col-md-3">
+                        <label for="temp_check">มาจาก</label>
+                        <input type="text" class="form-control" id="form" placeholder="จังหวัด" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="temp_check">เดินทางไป</label>
+                        <input type="text" class="form-control" id="to" placeholder="จังหวัด" value="">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="temp_check">อุณหภูมิปกติ</label>
+                        <select class="form-control" id="temp_check">
+                            <option value="ปกติ" > ปกติ</option>
+                            <option value="ไม่ปกติ"> ไม่ปกติ</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-3">
+                        <label for="temp_result">อุณหภูมิ</label>
+                        <input type="text" class="form-control" id="temp_result" placeholder="อุณหภูมิ" value="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="temp_result">อาการทั่วไป</label>
+                        <select class="form-control" id="symtom1">
+                            <option value="ปกติ" > ปกติ</option>
+                            <option value="ไม่ปกติ"> ไม่ปกติ</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-row col-md-4">
+                    <div class="form-group">
+                        <label for="check_point">จุดตรวจ</label>
+                        <input type="hidden" class="form-control" id="check_point" placeholder="จุดตรวจ" value="">
+                    <input type="text" class="form-control" id="check_point2" placeholder="จุดตรวจ" value="" disabled></div>
+                </div>
+
+                <div class="form-row col-md-12 text-center">
+                    <div class="form-group">
+                        <button type="button" class="btn btn-success btn-lg" id="btn_save">Save</button>
+                        <button type="button" class="btn btn-danger btn-lg" id="btn_close" data-dismiss="modal">Close</button>
+                    </div>
+
+                </div>
             </div>
-
             <!-- Modal footer -->
+
             <div class="modal-footer">
-                <button type="button" class="btn btn-success" id="btn_save">Save</button>
-                <button type="button" class="btn btn-danger" id="btn_close" data-dismiss="modal">Close</button>
             </div>
 
         </div>
@@ -142,14 +194,3 @@
 
 
 <script src="<?php echo base_url() ?>assets/apps/js/person_bypass.js" charset="utf-8"></script>
-
-<!--         foreach ($invit_type as $r) {
-                                if ($outsite["invit_type"] == $r->id) {
-                                    $s = "selected";
-                                } else {
-                                    $s = "";
-                                }
-                                echo "<option value=" $r->id" $s > $r->name </option>";
-
-}
--->
