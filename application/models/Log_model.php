e204faa8ad56322b77d12e9b20e85432b25bb4e5
kataynoi <thait-rex@hotmail.com>
2020-04-23 11:53:31 +0700

    add save log

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -20,7 +20,19 @@ class Log_model extends CI_Model
             ->set("d_update", date('Y-m-d H:m:s'))
             ->insert('log_download');
 
-        return $this->db->insert_id();
+        return $rs;
+
+    }
+    public function save_log_view($id, $name)
+    {
+
+        $rs = $this->db
+            ->set("user_id", $id)
+            ->set("item_name", $name)
+            ->set("d_update", date('Y-m-d H:m:s'))
+            ->insert('log_view');
+
+        return $rs;
 
     }
 }
